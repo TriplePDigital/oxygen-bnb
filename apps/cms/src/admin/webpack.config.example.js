48b78e9711318f8e169410b38a4d08e36d5a3c06
Dániel B. Papp <dpapp@odu.edu>
2023-01-21 16:43:32 -0500

    feat(cms): Created Strapi boilerplate

---
+++ apps/cms/src/admin/webpack.config.example.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* eslint-disable no-unused-vars */
+module.exports = (config, webpack) => {
+  // Note: we provide webpack above so you should not `require` it
+  // Perform customizations to webpack eslint-config-custom
+  // Important: return the modified eslint-config-custom
+  return config;
+};
